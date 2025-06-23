542c9a7b20154501722e9c054d3e8f06e0c4a038
chsidev <127224337+chsidev@users.noreply.github.com>
2025-06-23 16:49:53 +0700

    Update page.tsx

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import { useEffect, useState } from 'react';
+import { Suspense } from "react";
 import { useSearchParams } from 'next/navigation';
+import SuccessClient from "./SuccessClient";
 import Link from 'next/link';
 
 export const dynamic = "force-dynamic";
